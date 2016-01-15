adfddaa7df3547352fe40580ad718bd69ddd0c96
Blake Loring <blake_l@parsed.uk>
2016-01-15 00:40:25 +0000

    Open stream

--- prismatikapi/src/lib.rs
+++ prismatikapi/src/lib.rs
@@ -1,3 +1,14 @@
-#[test]
-fn it_works() {
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+pub struct Prismatik {
+	stream: TcpStream
+}
+
+impl Prismatik {
+	pub fn new(path: &str, key: &str) -> Prismatik {
+		Prismatik {
+			stream: TcpStream::connect(path).unwrap()
+		}
+	}
 }
