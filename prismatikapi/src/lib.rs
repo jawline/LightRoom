07144d07ef78ee4bcc86dca3265edaace9ed75ca
Blake Loring <blake_l@parsed.uk>
2016-01-15 00:44:56 +0000

    Send API key

--- prismatikapi/src/lib.rs
+++ prismatikapi/src/lib.rs
@@ -7,8 +7,15 @@ pub struct Prismatik {
 
 impl Prismatik {
 	pub fn new(path: &str, key: &str) -> Prismatik {
-		Prismatik {
+		let mut prism = Prismatik {
 			stream: TcpStream::connect(path).unwrap()
-		}
+		};
+		prism.send_key(key);
+		prism
+	}
+
+	pub fn send_key(&mut self, key: &str) {
+		let key_string = "apikey:{".to_string() + key + "}";
+		self.stream.write(&key_string.into_bytes());
 	}
 }
