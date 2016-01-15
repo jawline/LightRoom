920e1aaf385bccbc5de573ac051b1212d4c14be8
Blake Loring <blake_l@parsed.uk>
2016-01-15 01:13:32 +0000

    Shift a few status files, disable prismatik lib for prototype

--- core/src/fccore/core.rs
+++ core/src/fccore/core.rs
@@ -17,7 +17,7 @@ pub struct Core {
      */
     pub alive: bool,
 
-    prismatik: Prismatik,
+    //prismatik: Prismatik,
   
     /**
      * configuration for the core
@@ -36,10 +36,13 @@ impl Core {
         let config = Config::load(config_file);
         let mut core = Core {
             alive: true,
-            prismatik: Prismatik::new(&config.server_url, &config.api_key),
+        //    prismatik: Prismatik::new(&config.server_url, &config.api_key),
             log: Log::new(&format!("{}log{}", LOG_DIR, time::now().to_timespec().sec), config.log_config.log_limit),
             config: config
         };
+        //core.prismatik.set_brightness(75);
+        //core.prismatik.set_smooth(150);
+        //core.prismatik.set_all_lights(200, 0, 0);
         core
     }
 
